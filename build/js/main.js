bd2944ed5fed780fd29fe4dfe7d12b8ccd1df5db
Fanpeng Li <serforfp@gmail.com>
2023-10-04 14:33:47 +0100

    change the design

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,6 @@
+//dark mode switch
+
+//the mobile button
 const initApp = () => {
   const hamburgerBtn = document.getElementById("hamburger-button");
   const mobileMenu = document.getElementById("mobile-menu");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./build/**/*.{html,js}"],
+  darkmode: "class",
   theme: {
     extend: {
       colors: {
